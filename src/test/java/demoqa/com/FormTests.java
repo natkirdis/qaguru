831839ac3c14c621723f5fa133b0a994a6a66424
Natalia Kirdis <n.kirdis@propellerads.net>
2021-10-27 23:45:58 +0300

    positive form test added

--- src/test/java/demoqa/com/FormTests.java
+++ src/test/java/demoqa/com/FormTests.java
@@ -1,21 +1,103 @@
 package demoqa.com;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.junit5.SoftAssertsExtension;
+import demoqa.com.models.StudentData;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.Keys;
 
+import java.io.File;
+
+import static com.codeborne.selenide.AssertionMode.SOFT;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
+@ExtendWith({SoftAssertsExtension.class})
 public class FormTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.startMaximized = true;
+        Configuration.assertionMode = SOFT;
     }
+    //todo
+    //Поменять тип для хобби, предметов
 
     @Test
-    void fillFormTest() {
+    void positiveFillFormTest() {
+        StudentData student = new StudentData()
+                .withFirstName("Name")
+                .withLastName("LastName")
+                .withEmail("test@email.ru")
+                .withNumber("8888888888")
+                .withGender("Male")
+                .withYear("1990")
+                .withMonth("March")
+                .withDay("09")
+                .withSubjects("Maths")
+                .withHobbies("Sports")
+                .withImageName("img.png")
+                .withState("NCR")
+                .withCity("Delhi")
+                .withAddress("Some address");
+
         open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue(student.getFirstName());
+        $("#lastName").setValue(student.getLastName());
+        $("#userEmail").setValue(student.getEmail());
+        $(byText(student.getGender())).click();
+        $("#userNumber").setValue(student.getNumber());
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(student.getMonth());
+        $(".react-datepicker__year-select").selectOption(student.getYear());
+        $(".react-datepicker__day--009").click();
+        $("#submit").scrollTo();
+        $("#subjectsInput").setValue(student.getSubjects()).pressEnter();
+
+        $("#hobbiesWrapper").$(byText(student.getHobbies())).click();
+
+        $("#uploadPicture").uploadFile(new File(student.getImagePath()));
+        $("#currentAddress").setValue(student.getAddress());
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText(student.getState())).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText(student.getCity())).click();
+        $("#submit").click();
+
+        getValue("Student Name")
+                .shouldHave(text(student.getFirstName()
+                        + " " +student.getLastName()));
+        getValue("Student Email")
+                .shouldHave((text(student.getEmail())));
+        getValue("Gender")
+                .shouldHave((text(student.getGender())));
+        getValue("Mobile")
+                .shouldHave((text(student.getNumber())));
+        getValue("Date of Birth")
+                .shouldHave((text(student.getDay() + " " +
+                        student.getMonth() + "," +
+                        student.getYear())));
+        getValue("Subjects")
+                .shouldHave((text(student.getSubjects())));
+        getValue("Hobbies")
+                .shouldHave((text(student.getHobbies())));
+        getValue("Picture")
+                .shouldHave((text(student.getImageName())));
+        getValue("Address")
+                .shouldHave((text(student.getAddress())));
+        getValue("State and City")
+                .shouldHave((text(student.getState()
+                        + " " + student.getCity())));
+    }
+
+
+    private SelenideElement getValue(String fieldName) {
+        return $(".modal-content")
+                .$(byText(fieldName))
+                .sibling(0);
     }
 }
---
+++ src/test/java/demoqa/com/models/StudentData.java
@@ -0,0 +1,164 @@
+package demoqa.com.models;
+
+import java.util.Objects;
+
+public class StudentData {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String number;
+    private String gender;
+    private String year;
+    private String month;
+    private String day;
+    private String subjects; // todo Заменить на List
+    private String hobbies; // todo Заменить на List
+    private String imageName;
+    private String state;
+    private String city;
+    private String address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public StudentData withFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public StudentData withLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public StudentData withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public StudentData withNumber(String number) {
+        this.number = number;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public StudentData withGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    public String getSubjects() {
+        return subjects;
+    }
+
+    public StudentData withSubjects(String subjects) {
+        this.subjects = subjects;
+        return this;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public StudentData withHobbies(String hobbies) {
+        this.hobbies = hobbies;
+        return this;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public StudentData withImageName(String image) {
+        this.imageName = image;
+        return this;
+    }
+
+    public String getImagePath() {
+        return String.format("src/test/resources/%s", imageName);
+    }
+
+
+    public String getState() {
+        return state;
+    }
+
+    public StudentData withState(String state) {
+        this.state = state;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public StudentData withCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public StudentData withAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public StudentData withYear(String year) {
+        this.year = year;
+        return this;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public StudentData withMonth(String month) {
+        this.month = month;
+        return this;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public StudentData withDay(String day) {
+        this.day = day;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentData that = (StudentData) o;
+        return firstName.equals(that.firstName) && lastName.equals(that.lastName) && email.equals(that.email) && number.equals(that.number) && gender.equals(that.gender) && year.equals(that.year) && month.equals(that.month) && day.equals(that.day) && subjects.equals(that.subjects) && hobbies.equals(that.hobbies) && imageName.equals(that.imageName) && state.equals(that.state) && city.equals(that.city) && address.equals(that.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, email, number, gender, year, month, day, subjects, hobbies, imageName, state, city, address);
+    }
+}
